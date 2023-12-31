e82f1b6bde244611307fadd01bba7c6957dba54c
Timofey Maximov <moltenship7750@gmail.com>
2023-12-31 19:06:13 +0600

    Improve gallery

--- src/renderer/src/features/alerts/components/Alerts.tsx
+++ src/renderer/src/features/alerts/components/Alerts.tsx
@@ -1,4 +1,22 @@
 import { getAlertArrayBuffer } from '@renderer/api/alertsApi'
+import { Button } from '@renderer/components/ui/Button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@renderer/components/ui/Card'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from '@renderer/components/ui/pagination'
 import { useGetAlerts } from '@renderer/queries/useGetAlerts'
 
 type Props = {
@@ -8,21 +26,52 @@ type Props = {
 export const Alerts = ({ streamerId }: Props) => {
   const { data } = useGetAlerts(streamerId)
   return (
-    <div>
+    <div className="container flex flex-wrap gap-4">
       {data.map((memeAlert) => (
-        <div key={memeAlert.id}>
-          {memeAlert.name}
-          <img src={memeAlert.alertAnimatedPreview} alt="" />
-          <button
-            onClick={async () => {
-              const arrayBuffer = await getAlertArrayBuffer(memeAlert.alertUrl)
-              window.api.copy(arrayBuffer)
-            }}
-          >
-            test
-          </button>
-        </div>
+        <Card key={memeAlert.id} className="w-[40%]">
+          <CardHeader>
+            <CardTitle>{memeAlert.name}</CardTitle>
+            <CardDescription>{memeAlert.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <img className="h-36 w-36 object-contain" src={memeAlert.alertAnimatedPreview} alt="" />
+          </CardContent>
+          <CardFooter>
+            <Button
+              onClick={async () => {
+                const arrayBuffer = await getAlertArrayBuffer(memeAlert.alertUrl)
+                window.api.copy(arrayBuffer)
+              }}
+            >
+              Copy
+            </Button>
+          </CardFooter>
+        </Card>
       ))}
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#" isActive>
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">3</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   )
 }
