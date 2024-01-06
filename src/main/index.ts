f28d53e1cde9bb7e1f3a112a71f6b6d238b8f749
Timofey Maximov <moltenship7750@gmail.com>
2024-01-06 17:41:22 +0600

    Add icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,7 +11,7 @@ function createWindow(): void {
     title: 'memealerts',
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
